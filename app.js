f2859aaba3b1e7615b012a74e0bf7e4de385c9d5
Kendall Hayes <hayeskendall21@gmail.com>
2020-08-03 16:19:02 -0700

    refactoring JS. Added description data

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 //refactor api call function to be able to use same function for every call
 // ! ADD one call API one API call for all weather data you would need on one location 
 
-window.addEventListener('load',()=> {
+var weatherDataObj={};
+
+var currentConditions=  document.querySelector("header p");
+var tempHeader=  document.querySelector("header h2");
+var daysOfWeek=["Monday","Tuesday","Wednesday","Thursday","Friday"];
+
+function getData () {
+
     let longitude;
     let latitude;
-    let kelvinToFh;
-
+    
     if(navigator.geolocation){
         /* navigator object which is property of window object (contains document object too)
          is an object with properties written to obtain info about browser. One of those properties is .geolocation which returns a geolocation object with user locaion 
          If statement checks if user has accepted permission
          */
 
-         navigator.geolocation.getCurrentPosition( function (position) {
+         navigator.geolocation.getCurrentPosition( position => {
+            /* this parameter(position) is the object containing geolocation data that gets returned from getCurrentPosition()  */
+            
             longitude= position.coords.longitude;
-            /* this parameter(position) is the object that gets returned from getCurrentPosition()  */
             latitude= position.coords.latitude;
 
-            /* console.log(position); geolocation position data  */
+            console.log(position); /* geolocation position data  */
 
-            const API = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=4a9721a8067a91cc5de8f90d6c9d4c16`;
-            /* function for get request returns a object? */
+            const API = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely&appid=4a9721a8067a91cc5de8f90d6c9d4c16`;
+            /* function for requests/responses returns a promise that is a response object */
             fetch(API)
                 .then(response => {
                     /* then waits for data to return before executing. (response is arbitrary parameter for data returned from call) */
@@ -28,19 +35,42 @@ window.addEventListener('load',()=> {
 
                 })
                 .then(data => {
-                    console.log("we have data",data);
-
-                    kelvinToFh= Math.round((data.main.temp-273.15) * 9/5 + 32); 
-                    console.log(kelvinToFh);
-                    /* formula to convert kelvin to fahrenheit */
+                   weatherDataObj=data;
+                   console.log('recieved data', weatherDataObj);
                 
-                    document.querySelector("header h2").textContent = kelvinToFh;
-
                 })
-            
-         });
-
+            ;
+         }); 
+          
     } else {
         document.querySelector("header h1").textContent = "Broken"
     }
+
+}
+
+const getDay = () => {
+    var date = new Date();
+    var day = date.getDay()
+    console.log(day +"day nigga");
+}
+
+getDay();
+
+window.addEventListener('load',()=> {
+    getData();
+    
+    setTimeout(() => {
+        console.log(weatherDataObj, "Async Object")
+        var descriptionData=weatherDataObj.current.weather[0].description;
+        let kelvinToFh;
+
+        kelvinToFh= Math.round((weatherDataObj.current.temp-273.15) * 9/5 + 32);
+         /* formula to convert kelvin to fahrenheit */
+
+        tempHeader.textContent= kelvinToFh;
+        currentConditions.textContent=descriptionData;
+
+        
+    }, 5000);
+
 });
